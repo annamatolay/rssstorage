81b2199872964d2161ebfe25a8e4ac2019b04b7c
matolaypal <matolaypal@gmail.com>
2017-09-02 15:53:31 +0200

    dashboard created

---
+++ src/main/java/com/matolaypal/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.matolaypal.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ */
+@Controller
+@Scope("session")
+public class MainController {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
